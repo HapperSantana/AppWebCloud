45f2321c465cfc9c2a1d562e60825e8e0053841c
HapperSantana <happer.santana@gmail.com>
2025-05-01 23:38:36 -0600

    Septimo Commit

--- context/AppUtils.tsx
+++ context/AppUtils.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 import Loader from "@/components/Loader";
 import React, { createContext, useContext, useEffect, useState } from "react";
